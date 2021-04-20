90498abdfabba67dba7c541f0649609cdc53cedc
Bijay Tamang <lazon11@outlook.com>
2021-04-20 07:22:20 -0400

    repo section added

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ class App extends Component {
     user: {},
     loading: false,
     alert: null,
+    repos: [],
   };
   // async componentDidMount() {
   //   this.setState({ loading: true });
@@ -40,6 +41,14 @@ class App extends Component {
     );
     this.setState({ user: res.data, loading: false });
   };
+  //Get users repos
+  getUserRepos = async (username) => {
+    this.setState({ loading: true });
+    const res = await axios.get(
+      `https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+    this.setState({ repos: res.data, loading: false });
+  };
 
   //Clear users from state
   clearUsers = () => {
@@ -53,7 +62,7 @@ class App extends Component {
     }, 5000);
   };
   render() {
-    const { users, user, loading } = this.state;
+    const { users, user, loading, repos } = this.state;
     return (
       <Router>
         <div className="App">
@@ -84,7 +93,9 @@ class App extends Component {
                   <User
                     {...props}
                     getUser={this.getUser}
+                    getUserRepos={this.getUserRepos}
                     user={user}
+                    repos={repos}
                     loading={loading}
                   />
                 )}
